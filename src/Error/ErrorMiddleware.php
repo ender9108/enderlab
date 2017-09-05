a51d45e885b37f6006ac7d8f9c2361c9b13f2a73
Alexandre Berthelot <alexandre.berthelot@donzat.fr>
2017-09-05 10:41:48 +0200

    Update composer.json Update .gitignore

---
+++ src/Error/ErrorMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+namespace EnderLab\Error;
+
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ErrorMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var ResponseInterface
+     */
+    private $response;
+
+    /**
+     * ErrorMiddleware constructor.
+     * @param ResponseInterface $response
+     */
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param DelegateInterface $delegate
+     * @return ResponseInterface
+     * @throws \Exception
+     */
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
+    {
+        try {
+            $response = $delegate->process($request);
+
+            if (! $response instanceof ResponseInterface ) {
+                throw new \Exception('Application did not return a response', 500);
+            }
+        } catch (Throwable $e) {
+            $response = new Response($e->getCode(), [], $e->getMessage());
+        }
+
+        return $response;
+    }
+}
