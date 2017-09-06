24a669defd2e8212e39a448ef268142f4922b68c
Alexandre Berthelot <alexandre.berthelot@donzat.fr>
2017-09-06 15:41:42 +0200

    Update comment

--- src/Dispatcher/DispatcherMiddleware.php
+++ src/Dispatcher/DispatcherMiddleware.php
@@ -34,7 +34,7 @@ class DispatcherMiddleware implements MiddlewareInterface
      * Process an incoming server request and return a response, optionally delegating
      * to the next middleware component to create the response.
      *
-     * @param ServerRequestInterface  $request
+     * @param ServerRequestInterface $request
      * @param DelegateInterface      $delegate
      *
      * @return ResponseInterface
