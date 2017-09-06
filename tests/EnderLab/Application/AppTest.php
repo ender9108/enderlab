6198a458706cb446758c95d384ca3d28f4381919
Alexandre Berthelot <alexandre.berthelot@donzat.fr>
2017-09-06 13:07:05 +0200

    Update AppTest

--- tests/EnderLab/Application/AppTest.php
+++ tests/EnderLab/Application/AppTest.php
@@ -10,7 +10,6 @@ use EnderLab\Router\Router;
 use GuzzleHttp\Psr7\ServerRequest;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
-use InvalidArgumentException;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
@@ -139,7 +138,7 @@ class AppTest extends TestCase
         $this->assertInstanceOf(Route::class, $route);
         $route = $app->option('/', $this->makeMiddleware());
         $this->assertInstanceOf(Route::class, $route);
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(\InvalidArgumentException::class);
         $app->pouette('/', $this->makeMiddleware());
     }
 
