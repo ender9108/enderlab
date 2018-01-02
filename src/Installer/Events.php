f1f2ed0592e5b0a09ede1ad3232cb2f2d454e5f4
Alexandre Berthelot <alexandreberthelot9108@gmail.com>
2018-01-02 12:10:00 +0100

    Composer events create project

--- src/Installer/Events.php
+++ src/Installer/Events.php
@@ -11,8 +11,13 @@ class Events
         $event->getIO()->write('Test message "post install"');
     }
 
-    public static function postUpdatel(Event $event)
+    public static function postUpdate(Event $event)
     {
         $event->getIO()->write('Test message "post update"');
+    }
+
+    public static function postCreateProject(Event $event)
+    {
+        $event->getIO()->write('Test message "post create project"');
     }
 }
