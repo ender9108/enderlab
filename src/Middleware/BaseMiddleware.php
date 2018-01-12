2c6a261e00ab8869536721909e189dab4274330d
Alexandre Berthelot <alexandreberthelot9108@gmail.com>
2018-01-12 11:44:16 +0100

    Update travis.yml

---
+++ src/Middleware/BaseMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+namespace EnderLab\Middleware;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+abstract class BaseMiddleware implements MiddlewareInterface
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        throw new \Exception('You must overload this method !');
+    }
+}
