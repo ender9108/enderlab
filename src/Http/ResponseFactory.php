f0b5092e8bb2bdbac9a66a33865202ce2c814cce
Alexandre Berthelot <alexandreberthelot9108@gmail.com>
2018-01-17 16:23:18 +0100

    build by Makefile

--- src/Http/ResponseFactory.php
+++ src/Http/ResponseFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace EnderLab\MiddleEarth\Http;
 
 use GuzzleHttp\Psr7\Response;
@@ -11,8 +12,7 @@ class ResponseFactory
         array $headers = [],
         string $version = '1.1',
         string $reason = null
-    )
-    {
+    ) {
         $body = json_encode($body, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
 
         if (JSON_ERROR_NONE !== json_last_error()) {
@@ -26,7 +26,7 @@ class ResponseFactory
         return new Response(
             $status,
             $body,
-            array_merge($headers, array('Content-Type' => 'application/json')),
+            array_merge($headers, ['Content-Type' => 'application/json']),
             $version,
             $reason
         );
@@ -38,12 +38,11 @@ class ResponseFactory
         array $headers = [],
         string $version = '1.1',
         string $reason = null
-    )
-    {
+    ) {
         return new Response(
             $status,
             $body,
-            array_merge($headers, array('Content-Type' => 'text/plain; charset=utf-8')),
+            array_merge($headers, ['Content-Type' => 'text/plain; charset=utf-8']),
             $version,
             $reason
         );
@@ -55,12 +54,11 @@ class ResponseFactory
         array $headers = [],
         string $version = '1.1',
         string $reason = null
-    )
-    {
+    ) {
         return new Response(
             $status,
             $body,
-            array_merge($headers, array('Content-Type' => 'text/html; charset=utf-8')),
+            array_merge($headers, ['Content-Type' => 'text/html; charset=utf-8']),
             $version,
             $reason
         );
